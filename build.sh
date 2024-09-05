bad17bc455929f830a7b807d0f5fab1ac760226e
liuyang <308877565@qq.com>
2024-09-05 09:37:15 +0800

    update: 修改action

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ECR_Auth.sh
+
+image="${ecr_account}.dkr.ecr.${ecr_region}.${ecr_host}/web-vault:${tag}"
+
+
+function loginEcr() {
+  aws --profile=ChinaECR ecr --region cn-northwest-1 get-login --no-include-email | sh
+}
+
+function build() {
+  docker build . --file Dockerfile --tag "$image"
+}
+
+function push() {
+  docker push "$image"
+}
+
+loginEcr
+build
+push
