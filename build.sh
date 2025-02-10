0c461c804f943908dc15e9dad3c1b2e511f3d62c
reimuLy <41979703+reimuLy@users.noreply.github.com>
2025-02-10 15:57:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ecr_auth.sh
 
-image="${ecr_account}.dkr.ecr.${ecr_region}.${ecr_host}/web-vault:${server_tag}"
+image="${ecr_account}.dkr.ecr.${ecr_region}.${ecr_host}/dockerhub/vaultwarden/server:1.33.2-alpine-custom"
 
 
 function loginEcr() {
